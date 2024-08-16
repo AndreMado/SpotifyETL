4e822b66fdcffac12996beba89ac5cbeb3c54019
Andres Cortazar <andresxmaldonado@gmail.com>
2024-08-15 19:17:17 -0600

    Structure changes to implement a Docker container

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+# 1. Selecciona la imagen base de Airflow
+FROM apache/airflow:2.5.0
+
+# 2. Establece la variable de entorno para desactivar la creación de ejemplos en Airflow
+ENV AIRFLOW__CORE__LOAD_EXAMPLES=False
+
+# 3. Copia el archivo requirements.txt al contenedor
+COPY requirements.txt /requirements.txt
+
+# 4. Instala las dependencias adicionales que mencionas en requirements.txt
+RUN pip install --no-cache-dir -r /requirements.txt
+
+# 5. Copia los DAGs, plugins, y configuraciones al contenedor
+COPY dags/ /opt/airflow/dags/
+COPY plugins/ /opt/airflow/plugins/
+COPY config/ /opt/airflow/config/
+
+# 6. Exponer el puerto 8080 para acceder a la interfaz web de Airflow
+EXPOSE 8080
+
+# 7. Comando por defecto para iniciar el servidor web de Airflow
+CMD ["airflow", "webserver"]
---
+++ dags/spotify_dag.py
@@ -0,0 +1,33 @@
+from datetime import timedelta,datetime
+from airflow import DAG
+from airflow.utils.dates import days_ago
+from airflow.operators.python import PythonOperator
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past':False,
+    'start_date': datetime(2024,8,15),
+    'email': ['andresxmaldonado@gmail.com'],
+    'email_on_failure':True,
+    'email_on_retry':True,
+    'retries':1,
+    'retry_delay': timedelta(minutes=1)
+}
+
+dag = DAG(
+    'spotify_dag',
+    default_args = default_args,
+    description='Implemeting Airflow to my Spotify ETL process dag',
+    schedule_interval= timedelta(minutes=3)
+)
+
+def testing_fuction():
+    print("TESTINGGG")
+
+run_etl = PythonOperator(
+    task_id='whole_spotify_etl',
+    python_callable=testing_fuction,
+    dag=dag
+)
+
+run_etl
